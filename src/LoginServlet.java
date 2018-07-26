3c1fdc823548e4010e352c120c4e0265210203b4
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-26 15:30:55 +0700

    add login servlet with method doGet() to run web

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -8,7 +8,22 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//
+//        if ("admin".equals(username) && "admin".equals(password)) {
+//            writer.println("<h1>Welcome " + username + " to website</h1>");
+//        } else {
+//            writer.println("<h1>Login Error</h1>");
+//        }
+//        writer.println("</html>");
+//    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
@@ -20,10 +35,7 @@ public class LoginServlet extends HttpServlet {
         } else {
             writer.println("<h1>Login Error</h1>");
         }
+
         writer.println("</html>");
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
